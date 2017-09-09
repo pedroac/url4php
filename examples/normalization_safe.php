fe061aba092fe938672fca75a361df36e70f314d
pedroac <pedro.amaral.couto@gmail.com>
2017-09-10 00:38:11 +0100

    Change example

--- examples/normalization_safe.php
+++ examples/normalization_safe.php
@@ -5,4 +5,4 @@ use pedroac\url\URL;
 use pedroac\url\normalization\Rules;
 
 $rules = Rules::safe();
-echo $rules->apply(new URL('http://test.com/my/path?a=1&b=2&f=3')), "\n";
+echo $rules->apply(new URL('HttP://TesT.com/my/path?a=1&b=2&f=3')), "\n";
